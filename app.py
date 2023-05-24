995a14d3ba35012172df81762b86eb85ff1e5cd2
Sahaj Godhani <30712537+Sahaj777@users.noreply.github.com>
2023-05-24 17:54:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 from flask import Flask,request
 
@@ -16,25 +15,9 @@ def get_data():
     
     input_brand = data['brand_name']
     
-    chrome_options = Options()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--force-dark-mode")
-    chrome_options.add_argument("--start-maximized")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-infobars")
-
-    print('options defined')
-    capabilities = {
-        # "resolution": "2560X1440"
-        # "resolution": "1280X720"
-        "resolution": "768X432"
-    }
-
-    driver = webdriver.Chrome(executable_path="/opt/render/project/.render/chrome/opt/google/chrome/google-chrome/chromedriver",
-                               desired_capabilities=capabilities,options=chrome_options)
+    options = webdriver.ChromeOptions()
+    options.binary_location= '/usr/bin/google-chrome-stable'
+    driver = webdriver.Chrome(ChromeDriverManager().install(),chrome_options=options);
     
     url = "https://www.ajio.com/men-jeans/c/830216001?query=%3Arelevance&gridColumns=5"
    
